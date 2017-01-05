f4f18f5a2ebad236a49f19f37371fb14749a7727
Flavio Abe <flaviosabe@gmail.com>
2017-01-05 18:57:01 -0200

    Aggregate events by period

--- lib/event_processor.rb
+++ lib/event_processor.rb
@@ -3,26 +3,41 @@ require "aggregate_set"
 require "event"
 
 class EventProcessor
-  def initialize
+  # Initializes the EventProcessor to aggregate events every +interval+ seconds from the +start+ time.
+  def initialize(start=Time.parse("2017-01-01T00:00:00"), interval=60)
     @events = []
+    @start = start
+    @interval = interval
   end
 
+  # Records an event.
   def record(event)
     @events << event
   end
 
+  # Aggregate all events recorded since the last flush and return an AggregateSet for each period that has at least one recorded event.
   def flush
-    # ignores the timestamp for the time being
-    return [] if @events.empty?
+    events_by_period = @events.group_by { |event| ((event.timestamp - @start) / @interval).to_i }
 
-    aggregates_list = []
+    aggregate_sets = []
 
-    @events
-      .group_by { |event| event.type }
-      .each_pair { |type, events|
-        aggregates_list << Aggregate.new(type, events.map(&:value))
-      }
+    events_by_period.each_pair do |period, events|
+      aggregates_list = []
 
-    [ AggregateSet.new(aggregates_list, nil, nil) ]
+      events
+        .group_by { |event| event.type }
+        .each_pair { |type, events|
+          aggregates_list << Aggregate.new(type, events.map(&:value))
+        }
+
+      period_start = @start + @interval * period
+      period_end = @start + @interval * (period + 1)
+
+      aggregate_sets << AggregateSet.new(aggregates_list, period_start, period_end)
+    end
+
+    @events = []
+
+    aggregate_sets
   end
 end
--- spec/event_processor_spec.rb
+++ spec/event_processor_spec.rb
@@ -17,12 +17,15 @@ describe EventProcessor, "#aggregate" do
       @aggregate = @aggregates_list.first
     end
 
-    it "has a single aggregate" do
+    it "has a single aggregate set" do
       expect(@aggregates_list.size).to eq 1
     end
 
-    it "the aggregate has the event type" do
+    it "the aggregate set has a single aggregate" do
       expect(@aggregate.aggs.size).to eq 1
+    end
+
+    it "the aggregate has the event type" do
       expect(@aggregate.aggs.first.name).to eq "request_duration"
     end
 
@@ -43,25 +46,28 @@ describe EventProcessor, "#aggregate" do
     end
   end
 
-  context "with multiple events with the same type" do
+  context "with multiple events with the same type in the same period" do
     before do
       event_processor = EventProcessor.new
 
-      event_processor.record Event.new("request_duration", 10, Time.now)
-      event_processor.record Event.new("request_duration", 20, Time.now)
-      event_processor.record Event.new("request_duration", 30, Time.now)
-      event_processor.record Event.new("request_duration", 40, Time.now)
+      event_processor.record Event.new("request_duration", 10, Time.parse("2017-01-03T15:53:01.494821Z"))
+      event_processor.record Event.new("request_duration", 20, Time.parse("2017-01-03T15:53:05.494122Z"))
+      event_processor.record Event.new("request_duration", 30, Time.parse("2017-01-03T15:53:17.494145Z"))
+      event_processor.record Event.new("request_duration", 40, Time.parse("2017-01-03T15:53:42.000000Z"))
 
       @aggregates_list = event_processor.flush
       @aggregate = @aggregates_list.first
     end
 
-    it "has a single aggregate" do
+    it "has a single aggregate set" do
       expect(@aggregates_list.size).to eq 1
     end
 
-    it "the aggregate has the events type" do
+    it "the aggregate set has a single aggregate" do
       expect(@aggregate.aggs.size).to eq 1
+    end
+
+    it "the aggregate has the events type" do
       expect(@aggregate.aggs.first.name).to eq "request_duration"
     end
 
@@ -79,6 +85,104 @@ describe EventProcessor, "#aggregate" do
 
     it "the aggregate median value is the median event value" do
       expect(@aggregate.aggs.first.median).to eq 25
+    end
+  end
+
+  context "with multiple events with different types in the same period" do
+    before do
+      event_processor = EventProcessor.new
+
+      event_processor.record Event.new("request_duration", 10, Time.parse("2017-01-03T15:53:01.494821Z"))
+      event_processor.record Event.new("request_duration", 20, Time.parse("2017-01-03T15:53:05.494122Z"))
+      event_processor.record Event.new("request_count", 30, Time.parse("2017-01-03T15:53:17.494145Z"))
+      event_processor.record Event.new("request_count", 40, Time.parse("2017-01-03T15:53:42.000000Z"))
+
+      @aggregates_list = event_processor.flush
+      @aggregate = @aggregates_list.first
+    end
+
+    it "has a single aggregate set" do
+      expect(@aggregates_list.size).to eq 1
+    end
+
+    it "the aggregate set has one aggregate for each event type" do
+      expect(@aggregate.aggs.size).to eq 2
+
+      aggregate_names = @aggregate.aggs.map(&:name)
+      expect(aggregate_names).to include "request_duration"
+      expect(aggregate_names).to include "request_count"
+    end
+
+    it "the aggregate min value is the min event value for that type" do
+      expect(@aggregate.aggs.find { |a| a.name == "request_duration" }.min).to eq 10
+      expect(@aggregate.aggs.find { |a| a.name == "request_count" }.min).to eq 30
+    end
+
+    it "the aggregate max value is the max event value for that type" do
+      expect(@aggregate.aggs.find { |a| a.name == "request_duration" }.max).to eq 20
+      expect(@aggregate.aggs.find { |a| a.name == "request_count" }.max).to eq 40
+    end
+
+    it "the aggregate mean value is the mean event value for that type" do
+      expect(@aggregate.aggs.find { |a| a.name == "request_duration" }.mean).to eq 15
+      expect(@aggregate.aggs.find { |a| a.name == "request_count" }.mean).to eq 35
+    end
+
+    it "the aggregate median value is the median event value for that type" do
+      expect(@aggregate.aggs.find { |a| a.name == "request_duration" }.median).to eq 15
+      expect(@aggregate.aggs.find { |a| a.name == "request_count" }.median).to eq 35
+    end
+  end
+
+  context "with multiple events with the same type in different periods" do
+    before do
+      event_processor = EventProcessor.new
+
+      event_processor.record Event.new("request_duration", 10, Time.parse("2017-01-03T15:53:01.494821Z"))
+      event_processor.record Event.new("request_duration", 20, Time.parse("2017-01-03T15:53:05.494122Z"))
+
+      event_processor.record Event.new("request_duration", 30, Time.parse("2017-01-03T15:54:17.494145Z"))
+      event_processor.record Event.new("request_duration", 40, Time.parse("2017-01-03T15:54:42.000000Z"))
+
+      @aggregates_list = event_processor.flush
+      @first_aggregate_set, @second_aggregate_set = @aggregates_list
+    end
+
+    it "has an aggregate set for each period" do
+      expect(@aggregates_list.size).to eq 2
+    end
+
+    it "each aggregate set corresponds to a period" do
+      expect(@first_aggregate_set.period_start).to eq Time.parse("2017-01-03T15:53:00.000000Z")
+      expect(@first_aggregate_set.period_end).to eq Time.parse("2017-01-03T15:54:00.000000Z")
+
+      expect(@second_aggregate_set.period_start).to eq Time.parse("2017-01-03T15:54:00.000000Z")
+      expect(@second_aggregate_set.period_end).to eq Time.parse("2017-01-03T15:55:00.000000Z")
+    end
+
+    it "each aggregate set has a single aggregate" do
+      expect(@first_aggregate_set.aggs.size).to eq 1
+      expect(@second_aggregate_set.aggs.size).to eq 1
+    end
+
+    it "the aggregate min value is the min event value for that period" do
+      expect(@first_aggregate_set.aggs.first.min).to eq 10
+      expect(@second_aggregate_set.aggs.first.min).to eq 30
+    end
+
+    it "the aggregate max value is the max event value for that period" do
+      expect(@first_aggregate_set.aggs.first.max).to eq 20
+      expect(@second_aggregate_set.aggs.first.max).to eq 40
+    end
+
+    it "the aggregate mean value is the mean event value for that period" do
+      expect(@first_aggregate_set.aggs.first.mean).to eq 15
+      expect(@second_aggregate_set.aggs.first.mean).to eq 35
+    end
+
+    it "the aggregate median value is the median event value for that period" do
+      expect(@first_aggregate_set.aggs.first.median).to eq 15
+      expect(@second_aggregate_set.aggs.first.median).to eq 35
     end
   end
 end
